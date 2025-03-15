0bf1067f4454d6085e0a11720803535eabe353d6
anirban21100 <anirban21100@iiitnr.edu.in>
2025-03-15 21:02:19 +0530

    update imports

--- main_folder/code_base/pipeline/block_4/concat.py
+++ main_folder/code_base/pipeline/block_4/concat.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from keras import layers
 from code_base.pipeline.block_4.block_4 import Block4
-from code_base.pipeline.block_4.eca import ECALayer
+from code_base.pipeline.block_4.eca import ECALayer4
 from code_base.pipeline.block_3.dcl import DCL
 
 class Concat4(tf.keras.layers.Layer):
