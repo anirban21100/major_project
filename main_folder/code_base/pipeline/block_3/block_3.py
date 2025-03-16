6e67b9668a86693769ffd29b1f64875856327d6e
anirban21100 <anirban21100@iiitnr.edu.in>
2025-03-16 16:53:10 +0530

    update block layers

--- main_folder/code_base/pipeline/block_3/block_3.py
+++ main_folder/code_base/pipeline/block_3/block_3.py
@@ -15,7 +15,7 @@ class Block3(tf.keras.layers.Layer):
         super().__init__()
 
     def build(self):
-        self.conv2d = SeparableConv2D(
+        self.conv2d = [SeparableConv2D(
             64,
             (3, 3),
             strides=(1, 1),
@@ -23,21 +23,21 @@ class Block3(tf.keras.layers.Layer):
             depthwise_initializer="he_normal",
             pointwise_initializer="he_normal",
             use_bias=False,
-        )
+        ) for _ in range(3)]
         self.bn = BatchNormalization()
         self.leaky = LeakyReLU(alpha=0.2)
         self.add = Add()
 
     def call(self, inputs):
         x = inputs
-        layer1 = self.conv2d(x)
+        layer1 = self.conv2d[0](x)
         layer1 = self.bn(layer1)
         layer1 = self.leaky(layer1)
-        layer2 = self.conv2d(layer1)
+        layer2 = self.conv2d[1](layer1)
         layer2 = self.bn(layer2)
         layer2 = self.leaky(layer2)
         concat2 = self.add([layer1, layer2])
-        layer3 = self.conv2d(concat2)
+        layer3 = self.conv2d[2](concat2)
         layer3 = self.bn(layer3)
         layer3 = self.leaky(layer3)
         output = self.add([concat2, layer3])
