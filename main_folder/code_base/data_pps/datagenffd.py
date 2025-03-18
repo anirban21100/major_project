aa19d3a7487c64c9fc04a8169c325963299682b1
anirban21100 <anirban21100@iiitnr.edu.in>
2025-03-18 08:52:28 +0530

    update data_pps

--- main_folder/code_base/data_pps/datagenffd.py
+++ main_folder/code_base/data_pps/datagenffd.py
@@ -4,13 +4,13 @@ from tensorflow import keras
 from keras._tf_keras.keras.preprocessing.image import ImageDataGenerator
 
 class PreprocessData:
-    def transform(DIR, batch_size: int, shuffle: bool):
+    def transform(DIR, batch_size: int, shuffle: bool, class_mode=None):
         datagen = ImageDataGenerator(rescale=1 / 255.0)
 
         generator = datagen.flow_from_directory(
             DIR,
             batch_size=batch_size,
-            class_mode="categorical",
+            class_mode=class_mode, # make it categorical for train and validation dir
             target_size=(img_rows, img_cols),
             shuffle=shuffle,
         )
