4289c5c5b0c56ea49ece49c42bc8cc541cb79887
anirban21100 <anirban21100@iiitnr.edu.in>
2025-03-20 17:49:07 +0530

    update functions

--- main_folder/code_base/pipeline/postblock/post_block.py
+++ main_folder/code_base/pipeline/postblock/post_block.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.keras.layers import GlobalMaxPooling2D, Dense, Input, Dropout
+from tensorflow.keras.layers import GlobalMaxPooling2D, Dense, Input, Dropout, Softmax
 from code_base.utils.ArcFace import ArcMarginProduct
 
 class PostBlock(tf.keras.layers.Layer):
@@ -10,14 +10,18 @@ class PostBlock(tf.keras.layers.Layer):
     def build(self):
         self.globalpool = GlobalMaxPooling2D()
         self.dropout = Dropout(0.3)
+        self.label = Input(shape = ())
         self.dense = Dense(self.num_classes, activation="softmax")
         self.margin = ArcMarginProduct(
             n_classes=self.num_classes, s=30, m=0.5, dtype="float32"
         )
+        self.softmax = Softmax(dtype='float32')
 
     def call(self, inputs):
         x_offset = inputs
         x15 = self.globalpool(x_offset)
         x = self.dropout(x15)
-        output = self.dense(x)
+        x = self.margin([x, self.label])
+        output = self.softmax(x)
+        # self.dense(x)
         return output
